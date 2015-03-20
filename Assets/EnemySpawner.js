a2ab05009509dcd88cdc2cb76a739d7fc401e587
Alix Fumoleau <alix.fumoleau@gmail.com>
2015-03-20 02:12:16 -0400

    Add some randomness in enemy spawn position

--- Assets/EnemySpawner.js
+++ Assets/EnemySpawner.js
@@ -6,6 +6,7 @@ var particlesSpawn : ParticleSystem;
 var spawnLocations : Transform[];
 var goal : Transform;
 var enemyMaxCount : int;
+private var offset = new Vector3(0.1, 0.0, 0.0);
 
 private var clock : float;
 
@@ -20,7 +21,8 @@ function Update ()
 	{
 		clock = 0.0;
 		var spawn = Instantiate(enemyPrefab).transform;
-		spawn.position = spawnLocations[Mathf.Floor(Random.value * spawnLocations.length)].position;
+		spawn.position = spawnLocations[Mathf.Floor(Random.value * spawnLocations.length)].position + offset;
+		offset = -offset;
 		Instantiate(particlesSpawn,spawn.position,Quaternion.Euler(-90,0,0));
 		spawn.GetComponent.<Enemy>().goal = goal;
 		spawn.parent = transform;

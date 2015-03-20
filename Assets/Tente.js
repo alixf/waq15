77e93b3b9bab16020cc320c4e568c5faa423466a
Alix Fumoleau <alix.fumoleau@gmail.com>
2015-03-20 18:44:55 -0400

    Add tent animation

---
+++ Assets/Tente.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+public var target : Health;
+
+function Start ()
+{
+
+}
+
+function Update ()
+{
+	var h : float = target.health;
+	var mh :float = target.maxHealth;
+	GetComponent.<Animator>().SetFloat("light", 1-(h/mh));
+}

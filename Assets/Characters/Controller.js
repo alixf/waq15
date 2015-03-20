9a4b0af1f6747de3cd66bec340ed81a5bd2fc5ca
Alix Fumoleau <alix.fumoleau@gmail.com>
2015-03-20 14:28:09 -0400

    Remove trigger control for shooting, now using right stick instead.

--- Assets/Characters/Controller.js
+++ Assets/Characters/Controller.js
@@ -2,23 +2,43 @@
 
 public var speed = 1.0;
 public var controllerId = 0;
+public var created = false;
 
+private var y;
 function Start()
 {
+	this.y = transform.position.y;
+	transform.position.y = -10000;
 }
 
 function FixedUpdate()
 {
-	var stick1 = new Vector2(Input.GetAxis("P"+controllerId+" A1"), Input.GetAxis("P"+controllerId+" A2"));
-	var stick2 = new Vector2(Input.GetAxis("P"+controllerId+" A4"), Input.GetAxis("P"+controllerId+" A5"));
-	
-	if(stick2.magnitude > 0.0)
-		transform.eulerAngles.y = Mathf.Atan2(stick2.x, stick2.y) * Mathf.Rad2Deg;
-	if(stick1.magnitude > 0.0)
+	if(!created)
 	{
-		var direction = new Vector3(stick1.x, 0.0, stick1.y);
-		GetComponent.<Rigidbody>().velocity = direction.normalized * speed;
+		if (Input.GetAxis("P"+controllerId+" A1") != 0.0f ||
+			Input.GetAxis("P"+controllerId+" A2") != 0.0f ||
+			Input.GetAxis("P"+controllerId+" A3") != 0.0f ||
+			Input.GetAxis("P"+controllerId+" A4") != 0.0f ||
+			Input.GetAxis("P"+controllerId+" A5") != 0.0f ||
+			Input.GetButtonDown("P"+controllerId+" Start"))
+		{
+			created = true;
+			transform.position.y = this.y;
+		}
 	}
 	else
-		GetComponent.<Rigidbody>().velocity = Vector3.zero;
+	{
+		var stick1 = new Vector2(Input.GetAxis("P"+controllerId+" A1"), Input.GetAxis("P"+controllerId+" A2"));
+		var stick2 = new Vector2(Input.GetAxis("P"+controllerId+" A4"), Input.GetAxis("P"+controllerId+" A5"));
+		
+		if(stick2.magnitude > 0.0)
+			transform.eulerAngles.y = Mathf.Atan2(stick2.x, stick2.y) * Mathf.Rad2Deg;
+		if(stick1.magnitude > 0.0)
+		{
+			var direction = new Vector3(stick1.x, 0.0, stick1.y);
+			GetComponent.<Rigidbody>().velocity = direction.normalized * speed;
+		}
+		else
+			GetComponent.<Rigidbody>().velocity = Vector3.zero;
+	}
 }

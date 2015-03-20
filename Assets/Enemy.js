31957b19001f3c4502f5d8f237009a70cd9b9247
Alix Fumoleau <alix.fumoleau@gmail.com>
2015-03-20 02:02:50 -0400

    Enemies now hit the goal !

--- Assets/Enemy.js
+++ Assets/Enemy.js
@@ -2,6 +2,8 @@
 
 var goal : Transform;
 var speed : float;
+private var hitClock = 0.0;
+var hitDuration	 = 0.0;
 
 function Start()
 {
@@ -9,6 +11,7 @@ function Start()
 
 function Update()
 {
+	hitClock += Time.deltaTime;
 	var direction = (goal.position - transform.position);
 	direction.y = 0;
 	
@@ -17,4 +20,13 @@ function Update()
 
 function OnTriggerStay(collider : Collider)
 {
+	if(collider.CompareTag("hitZone"))
+	{
+		if(hitClock > hitDuration)
+		{
+			hitClock = 0.0;
+			collider.transform.parent.GetComponent.<Health>().health--;
+			print("ok");
+		}
+	}
 }
